18939c7e7d77fa436c5448aa498b8f35c16da630
Replit user <>
2023-05-25 00:26:40 +0000

    create loan

--- CreateLoans.java
+++ CreateLoans.java
@@ -1,23 +1,34 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class CreateLoans {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What is your current prime interest rate?");
-        double currentInterestRate;
+    System.out.println("What is your current prime interest rate?");
+    double currentInterestRate;
 
-        while (true) {
-            if (scanner.hasNextDouble()) {
-                currentInterestRate = scanner.nextDouble();
-                break;
-            } else {
-                System.out.println("Invalid input. Please enter a valid interest rate.");
-                scanner.next(); 
-            }
-        }
-        
-
-
+    while (true) {
+      if (scanner.hasNextDouble()) {
+        currentInterestRate = scanner.nextDouble();
+        break;
+      } else {
+        System.out.println("Invalid input. Please enter a valid interest                                 rate.");
+        scanner.next();
+      }
     }
+
+    System.out.println("Would you like a personal personal or business                               loan?");
+    String loanType;
+
+    while (true) {
+      if (scanner.hasN) {
+        currentInterestRate = scanner.nextDouble();
+        break;
+      } else {
+        System.out.println("Invalid input. Please enter personal or                                     business.");
+        scanner.next();
+      }
+    }
+
+  }
 }
--- Loan.java
+++ Loan.java
@@ -1,42 +1,40 @@
 public abstract class Loan implements LoanConstants {
-    
-    protected int loanNumber, termLength;
-    protected double loanAmount, interestRate;
-    protected String customerLastName;
-    
 
-    public Loan (int loanNumber, int termLength, double loanAmount, String customerLastName){
-            this.loanNumber = loanNumber;
-            this.termLength = termLength;
-            this.loanAmount = loanAmount;
-            this.customerLastName = customerLastName;
-    }
+  protected int loanNumber, termLength;
+  protected double loanAmount, interestRate;
+  protected String customerLastName;
 
+  public Loan(int loanNumber, int termLength, double loanAmount, String customerLastName) {
+    this.loanNumber = loanNumber;
+    this.termLength = termLength;
+    this.loanAmount = loanAmount;
+    this.customerLastName = customerLastName;
+  }
 
-    protected double validateLoanAmount(double loanAmount){
-        if(loanAmount > LoanConstants.maxLoanAmount){
-        throw new IllegalArgumentException("Maximum loam amount is" + LoanConstants.maxLoanAmount);
+  protected double validateLoanAmount(double loanAmount) {
+    if (loanAmount > LoanConstants.maxLoanAmount) {
+      throw new IllegalArgumentException("Maximum loam amount is" + LoanConstants.maxLoanAmount);
     }
     return maxLoanAmount;
+  }
+
+  protected int validateTermLength(int termLength) {
+    if (termLength != LoanConstants.shortTerm && termLength != LoanConstants.mediumTerm
+        && termLength != LoanConstants.longTerm) {
+      throw new IllegalArgumentException("Invalid loan term");
     }
-    
+    return LoanConstants.shortTerm;
+  }
 
-    protected int validateTermLength (int termLength){
-        if(termLength != LoanConstants.shortTerm && termLength != LoanConstants.mediumTerm && termLength != LoanConstants.longTerm){
-            throw new IllegalArgumentException("Invalid loan term");
-        }
-        return LoanConstants.shortTerm;
-    }
+  // public boolean eaquals(Object loan){
+  // if(loan.loanAmount == )
+  // }
 
-    // public boolean eaquals(Object loan){
-    //     if(loan.loanAmount == )
-    // } 
-
-    @Override
-    public String toString() {
+  @Override
+  public String toString() {
     return "Loan Number: " + this.loanNumber + "\n"
-         + "Loan Term: " + this.termLength + "\n"
-         + "Loan Amount: " + this.loanAmount + "\n"
-         + "Customer: " + this.customerLastName;
-}
+        + "Loan Term: " + this.termLength + "\n"
+        + "Loan Amount: " + this.loanAmount + "\n"
+        + "Customer: " + this.customerLastName;
+  }
 }
